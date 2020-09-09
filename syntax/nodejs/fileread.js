452c6ef197bb00ff6fbc0c4cb1acc123caae5e06
azurchief <67046863+azurchief@users.noreply.github.com>
2020-09-09 23:40:33 +0900

    file reading (linking the txt file)
    let main.js to read the txt file contents to display them all. (Hence, this file can reduce the size of its file and reduce some burden.)

--- syntax/nodejs/fileread.js
+++ syntax/nodejs/fileread.js
@@ -2,3 +2,4 @@ var fs = require("fs");
 fs.readFile("sample.txt", "utf8", function (err, data) {
   console.log(data);
 });
+// dir // input this word to see what files are in that current directory location. (from parent to child) that's it!
