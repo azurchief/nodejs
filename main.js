651e1f2a6c296f35600dcb98e596a8d62b0099c8
azurchief <67046863+azurchief@users.noreply.github.com>
2020-09-11 17:29:46 +0900

    understood!

--- main.js
+++ main.js
@@ -12,7 +12,6 @@ var app = http.createServer(function (request, response) {
       fs.readdir("./data", function (error, filelist) {
         var title = "Welcome";
         var description = "Hello, Node.js";
-        var list = "<ul>";
         /*  <ul>
         <li><a href="/?id=HTML">HTML</a></li>
         <li><a href="/?id=CSS">CSS</a></li>
@@ -21,7 +20,6 @@ var app = http.createServer(function (request, response) {
 */
 
         var list = "<ul>";
-
         var i = 0;
         while (i < filelist.length) {
           list =
@@ -50,9 +48,37 @@ var app = http.createServer(function (request, response) {
       });
       ////
     } else {
-      fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
-        var title = queryData.id;
-        var template = `
+      /* copied same code from above paragraph */
+      /* since this is 'else' sector,  'if' is no longer needed down here. */
+      fs.readdir("./data", function (error, filelist) {
+        var title = "Welcome";
+        var description = "Hello, Node.js";
+        var list = "<ul>";
+        /*  <ul>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ul>";
+    */
+        var list = "<ul>";
+
+        var i = 0;
+        while (i < filelist.length) {
+          list =
+            list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+          i = i + 1;
+        }
+
+        list = list + "</ul>";
+        /* end of copy from above paragraph */
+
+        /* starting point : original 2nd paragraph */
+        fs.readFile(`data/${queryData.id}`, "utf8", function (
+          err,
+          description
+        ) {
+          var title = queryData.id;
+          var template = `
         <!doctype html>
         <html>
         <head>
@@ -61,18 +87,16 @@ var app = http.createServer(function (request, response) {
         </head>
         <body>
           <h1><a href="/">WEB</a></h1>
-          <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ul>
+        ${list}
           <h2>${title}</h2>
           <p>${description}</p>
         </body>
         </html>
         `;
-        response.writeHead(200);
-        response.end(template);
+          response.writeHead(200);
+          response.end(template);
+        });
+        /* end of original 2nd paragraph */
       });
     }
   } else {
@@ -82,6 +106,6 @@ var app = http.createServer(function (request, response) {
 });
 app.listen(3000);
 
-// if stucked,
-//go https://opentutorials.org/module/3549/21046 + https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback (just for referencing purpose)
-//+ https://opentutorials.org/course/3332/21123
+/* if stuck,
+go https://opentutorials.org/module/3549/21046 + https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback (just for referencing purpose)
++ https://opentutorials.org/course/3332/21123 */
