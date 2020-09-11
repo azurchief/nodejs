051921e04dd869b964d30692e3a9e1ed1dcc645d
azurchief <67046863+azurchief@users.noreply.github.com>
2020-09-11 22:45:55 +0900

    important - must review
    important - must review!
    (kinda refactoring process. Mainly used function (like class thing in html/css) to minimise all repeatitive code.

--- main.js
+++ main.js
@@ -2,6 +2,35 @@ var http = require("http");
 var fs = require("fs");
 var url = require("url");
 
+function templateHTML(title, list, body) {
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+  ${list}
+  ${body}
+  </body>
+  </html>
+  `;
+}
+
+function templateList(filelist) {
+  var list = "<ul>";
+  var i = 0;
+  while (i < filelist.length) {
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    i = i + 1;
+  }
+
+  list = list + "</ul>";
+  return list;
+}
+
 var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
@@ -12,37 +41,12 @@ var app = http.createServer(function (request, response) {
       fs.readdir("./data", function (error, filelist) {
         var title = "Welcome";
         var description = "Hello, Node.js";
-        /*  <ul>
-        <li><a href="/?id=HTML">HTML</a></li>
-        <li><a href="/?id=CSS">CSS</a></li>
-        <li><a href="/?id=JavaScript">JavaScript</a></li>
-      </ul>";
-*/
-
-        var list = "<ul>";
-        var i = 0;
-        while (i < filelist.length) {
-          list =
-            list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-          i = i + 1;
-        }
-
-        list = list + "</ul>";
-        var template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            ${list}
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-          `;
+        var list = templateList(filelist);
+        var template = templateHTML(
+          title,
+          list,
+          `<h2>${title}</h2>${description}`
+        );
         response.writeHead(200);
         response.end(template);
       });
@@ -51,48 +55,17 @@ var app = http.createServer(function (request, response) {
       /* copied same code from above paragraph */
       /* since this is 'else' sector,  'if' is no longer needed down here. */
       fs.readdir("./data", function (error, filelist) {
-        var title = "Welcome";
-        var description = "Hello, Node.js";
-        var list = "<ul>";
-        /*  <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ul>";
-    */
-        var list = "<ul>";
-
-        var i = 0;
-        while (i < filelist.length) {
-          list =
-            list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-          i = i + 1;
-        }
-
-        list = list + "</ul>";
-        /* end of copy from above paragraph */
-
-        /* starting point : original 2nd paragraph */
         fs.readFile(`data/${queryData.id}`, "utf8", function (
           err,
           description
         ) {
           var title = queryData.id;
-          var template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-        ${list}
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-        </html>
-        `;
+          var list = templateList(filelist);
+          var template = templateHTML(
+            title,
+            list,
+            `<h2>${title}</h2>${description}`
+          );
           response.writeHead(200);
           response.end(template);
         });
